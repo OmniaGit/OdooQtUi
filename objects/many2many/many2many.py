74fe4523237a3cb71802a05f98eb420d7b7477ff
Daniel Smerghetto <re_dan@hotmail.it>
2017-02-09 18:47:12 +0100

    Improved many2many

--- objects/many2many/many2many.py
+++ objects/many2many/many2many.py
@@ -16,4 +16,21 @@ class Many2many(OdooFieldTemplate):
         self.hboxLay = self.getQtObject()
 
     def getQtObject(self):
-        pass
+        self.hboxLay = QtGui.QVBoxLayout()
+        buttonsLay = QtGui.QHBoxLayout()
+        self.labelQtObj = QtGui.QLabel(self.labelString)
+        buttonsLay.addWidget(self.labelQtObj)
+        createButt = QtGui.QPushButton('Create')
+        editButton = QtGui.QPushButton('Edit')
+        addButton = QtGui.QPushButton('Add')
+        removeButton = QtGui.QPushButton('Remove')
+        buttonsLay.addWidget(createButt)
+        buttonsLay.addWidget(editButton)
+        buttonsLay.addWidget(addButton)
+        buttonsLay.addWidget(removeButton)
+        buttonsLay.addSpacerItem(QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum))
+        self.hboxLay.addLayout(buttonsLay)
+        self.widgetQtObj = QtGui.QTableWidget()
+        self.widgetQtObj.setToolTip(self.fieldDefinition.get('help', ''))
+        self.hboxLay.addWidget(self.widgetQtObj)
+        return self.hboxLay
