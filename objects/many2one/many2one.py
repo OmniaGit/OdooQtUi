a565b3426a33c63ce6fae4dfab27d744de55ee8b
Daniel Smerghetto <re_dan@hotmail.it>
2017-02-09 18:30:04 +0100

    Added many2one

--- objects/many2one/many2one.py
+++ objects/many2one/many2one.py
@@ -13,7 +13,19 @@ class Many2one(OdooFieldTemplate):
         super(Many2one, self).__init__(xmlField, fieldsDefinition)
         self.labelQtObj = False
         self.widgetQtObj = False
+        self.availableItems = self.getItems()
+        self.availableItems.append('Create and Edit...')
         self.hboxLay = self.getQtObject()
 
+    def getItems(self):
+        return ['']
+        
     def getQtObject(self):
-        pass
+        self.hboxLay = QtGui.QHBoxLayout()
+        self.labelQtObj = QtGui.QLabel(self.labelString)
+        self.hboxLay.addWidget(self.labelQtObj)
+        self.widgetQtObj = QtGui.QComboBox()
+        self.widgetQtObj.addItems(self.availableItems)
+        self.widgetQtObj.setToolTip(self.fieldDefinition.get('help', ''))
+        self.hboxLay.addWidget(self.widgetQtObj)
+        return self.hboxLay
