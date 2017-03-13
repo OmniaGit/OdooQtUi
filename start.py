b21cb8604d24cf2ed71b1cd2f4a9b02632f681c5
dsmerghetto <daniel.smerghetto@omniasolutions.eu>
2017-03-13 14:17:24 +0100

    Layout

--- start.py
+++ start.py
@@ -104,13 +104,13 @@ if __name__ == '__main__':
     dbName = 'all_v9_2'
     loginType = 'xmlrpc'
 
-#     scheme = 'http'
-#     xmlrpcServerIP = 'www.odooplm.cloud'
-#     xmlrpcPort = 8066
-#     user = 'odooplm'
-#     password = 'odooplm'
-#     dbName = 'odoov9_0'
-#     loginType = 'xmlrpc'
+    scheme = 'http'
+    xmlrpcServerIP = 'www.odooplm.cloud'
+    xmlrpcPort = 8066
+    user = 'odooplm'
+    password = 'odooplm'
+    dbName = 'odoov9_0'
+    loginType = 'xmlrpc'
 
 #     scheme = 'http'
 #     xmlrpcServerIP = '127.0.0.1'
@@ -127,10 +127,10 @@ if __name__ == '__main__':
 
     # dialog = QtGui.QDialog(None, QtCore.Qt.WindowSystemMenuHint | QtCore.Qt.WindowTitleHint)
     dialog = QtGui.QDialog()
-    templateViewObj = connectorObj.initViewObj('form', 'sale.order', '', False, useHeader=False)
+    templateViewObj = connectorObj.initViewObj('form', 'product.product', '', False, useHeader=False)
     qtInterface = templateViewObj.QtInterface
     # objIds = [77540]
-    objIds = [1]
+    objIds = [127]
     # startingFieldValues = {'description': 'non-settare'}
     startingFieldValues = {}
     readonlyFields = {}     # {'description': True}
